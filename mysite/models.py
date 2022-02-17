a41f1386484a53323f8320246d639ac3c2484ec4
trando46 <trando46@gmail.com>
2022-02-16 18:24:10 -0800

    Adding the functions to the AddModel class

--- mysite/models.py
+++ mysite/models.py
@@ -1,16 +1,36 @@
 from django.db import models
 
+# This is the model class for the address 
 class AddModel(models.Model):
-    street=models.CharField(max_length=100)
-    city= models.CharField(max_length=100)
-    state=models.CharField(max_length=100)
-    street=models.CharField(max_length=100)
-    state_iso= models.CharField(max_length=100)
-    country=models.CharField(max_length=100)
-    country_iso=models.CharField(max_length=100)
     
-    postal_code=models.IntegerField()
+    #### This need to be removed later 
+    #street=models.CharField(max_length=100)
+    #city= models.CharField(max_length=100)
+    #state=models.CharField(max_length=100)
+    #street=models.CharField(max_length=100)
+    #state_iso= models.CharField(max_length=100)
+    #country=models.CharField(max_length=100)
+    #country_iso=models.CharField(max_length=100)
+    #postal_code=models.IntegerField()
     
+    def getStreet():
+        return models.CharField(max_length=100) 
+
+    def getCity():
+        return models.CharField(max_length=100)
     
-    class Meta:
-        db_table="Adress"
+    def getState():
+        return models.CharField(max_length=100)
+    
+    def getStateISO():
+        return models.CharField(max_length=100)
+    
+    def getCountry():
+        return models.CharField(max_length=100)
+    
+    def getCountryISO():
+        return models.CharField(max_length=100)
+    
+    def getPostalCode():
+        return models.CharField(max_length=100)
+
