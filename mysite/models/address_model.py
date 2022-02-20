b6c033c3db430e95cb72e5f56e52a4f54dff6441
trando46 <trando46@gmail.com>
2022-02-20 11:02:47 -0800

    Adding the self parameter to the methods

--- mysite/models/address_model.py
+++ mysite/models/address_model.py
@@ -8,36 +8,25 @@ class
 class AddressModel(models.Model):
     #TODO: Need a method for getting a full address, not just parts of an address
     #TODO: We should have a constructor no?
-    
-    #### This need to be removed later 
-    #street=models.CharField(max_length=100)
-    #city= models.CharField(max_length=100)
-    #state=models.CharField(max_length=100)
-    #street=models.CharField(max_length=100)
-    #state_iso= models.CharField(max_length=100)
-    #country=models.CharField(max_length=100)
-    #country_iso=models.CharField(max_length=100)
-    #postal_code=models.IntegerField()
 
-    # TODO: This is not completed, needs self as a parameter.
-    def getStreet():
+    def getStreet(self):
         return models.CharField(max_length=100) 
 
-    def getCity():
+    def getCity(self):
         return models.CharField(max_length=100)
     
-    def getState():
+    def getState(self):
         return models.CharField(max_length=100)
     
-    def getStateISO():
+    def getStateISO(self):
         return models.CharField(max_length=100)
     
-    def getCountry():
+    def getCountry(self):
         return models.CharField(max_length=100)
     
-    def getCountryISO():
+    def getCountryISO(self):
         return models.CharField(max_length=100)
     
-    def getPostalCode():
+    def getPostalCode(self):
         return models.CharField(max_length=100)
 
