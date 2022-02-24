5ee12e6d6ec7799bf5cd9212766fb960d7daef8c
trando46 <trando46@gmail.com>
2022-02-23 17:13:04 -0800

    adding a link to help with programming this class

--- postalAddress/forms.py
+++ postalAddress/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from postal_address.models.address_model import AddressModel
-
+# https://www.javatpoint.com/django-forms#:~:text=Django%20provides%20a%20Form%20class,how%20it%20works%20and%20appears.&text=Each%20field%20of%20the%20form,validation%20while%20submitting%20the%20form.
 #
 class Addforms(forms.ModelForm):
     class Meta:
