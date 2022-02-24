3559af74b6fa46dcb9d1b86e08ced1263f5b3d99
trando46 <trando46@gmail.com>
2022-02-24 15:05:37 -0800

    Adding rendertemplate stuff to the home page to render the html inputs. Note for now this is temporary still need to connect to other stuff such as retrieving what the user input

--- manage.py
+++ manage.py
@@ -2,7 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from flask import Flask,render_template,request
+app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template(("home.html"))
+
+@app.route("/result",methods=["POST","GET"])
+def result():
+    output = request.form.to_dict()
+    name = output["name"]
+    return render_template("home.html",name = name)
 
 def main():
     """Run administrative tasks.
@@ -24,5 +36,9 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+
+
 if __name__ == '__main__':
-    main()
+    #main()
+    app.run(debug=True, port=2000)
+
