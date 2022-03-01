2460338cd359f9c2f2737d9da0febacbbf17913e
Alicia Garcia <garcia.alicia1990@gmail.com>
2022-02-28 18:15:51 -0800

    Updated models to create tables for our DB based on design

--- postalAddress/models.py
+++ postalAddress/models.py
@@ -1,55 +1,54 @@
 from django.db import models
-import entry_type_enum
 
 # Create your models here.
 """"
 Create a table for holding the address structure of a country with the country name, 
-country iso code and the address format. The address_format is a JSON string we 
-collect from the API and store as it's own field, converted to an Array field based 
-on the FormatFields Model to extract the correct structure and address references
-
-EX: JP -> Postal, Prefecture, Municipal, Address Line 2, Address Line 1.
-EX: MX -> Address Line, Settlement, Postal, Municipality, Federal Entity
+country iso code and the address format. The address_format is a string we 
+collect from the API and store as its own field. This will be used for dictating how new 
+addresses must be entered for a given country when the API is called. 
 """
-class AddressStructure(models.Model):
+
+
+class CountryAddressStructure(models.Model):
+    country_id = models.IntegerField(primary_key=True)
     country_name = models.CharField(max_length=100)
     country_iso = models.CharField(max_length=100)
-    address_format = models.JSONField()
+    address_format = models.CharField(max_length=250)  # store the structure of the country's address as a string separated by commas
+
 
 """
-This class model will create the JSON string to hold the format
-of the country address
+Create table for addresses based on country_id FK. AddressLine will hold the postal_code, state, city, and street in the 
+order they are expected for the given country as a JSON field.
 """
-class FormatFields:
-    # What type of entry in the form this will be. Default is default(0)
-    field_type = entry_type_enum.FormEntry.DEFAULT
 
-    # key value for referencing the address field by the correct name. Default is empty
-    key = ""
 
-    # Name of the address field to display to user. Default is empty
-    display_name = ""
+class Addresses(models.Model):
+    address_id = models.IntegerField(primary_key=True)
+    country_id = models.ForeignKey(CountryAddressStructure, on_delete=models.CASCADE)  # if the FK is deleted, this enter is deleted
+    addressLine = models.JSONField()
 
-    # bool for whether or not the field is required. Default is true
-    optional = True
-
-    def __init__(self, type, key, display, optional):
-        self.field_type = type
-        self.key = key
-        self.display_name = display
-        self.optional = optional
 
 """
-Create a table for storing all country's states/prefectures/provinces/etc.
-
-Using State as our variable to reference the values for each country's version,
-but display name will be dependent on the key from address_format_model.py as we require 
-this data to create a new country
+This class model will allow for easier access of the JSON information in the Addresses table.
+You can call the keys of the address (state, postal_code, etc.) and return the values.
+All default values are empty strings
 """
-class CountryTerritories(models.Model):
-    country = models.CharField(max_length=100)
-    country_iso = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    #meta is for database table connection
-    class Meta:
-        db_table="sample1"
+
+class AddressLine:
+    # key for postal code for any given address
+    postal_code = ""
+
+    # key for state for any given address
+    state = ""
+
+    # key for city for any given address
+    city = ""
+
+    # key for street for any given address. This field will contain the sections of the address that aren't covered by the above
+    street = ""
+
+    def __init__(self, postal_code, state, city, street):
+        self.postal_code = postal_code
+        self.state = state
+        self.city = city
+        self.street = street
