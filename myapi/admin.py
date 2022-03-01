0f135ddcd4ca01abc85280adb40b99df117cc3e1
Alicia Garcia <garcia.alicia1990@gmail.com>
2022-02-28 20:06:12 -0800

    Registered the DBs with admin.py so we can add data through the webpage.

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CountryAddressStructure
+from .models import Addresses
 
 # Register your models here.
+"""
+Adding our DB tables to our admin UI in Django
+"""
+
+admin.site.register(CountryAddressStructure)
+admin.site.register(Addresses)
