554e4c0a9ffedb75252699a729bb45a780a4e150
trando46 <trando46@gmail.com>
2022-03-02 23:19:25 -0800

    adding the necessary files to display our ui

--- myapi/views.py
+++ myapi/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Addresses, CountryAddressStructure
+
+def index(request):
+    """View function for home page of site"""
+    # Generate counts of the addresses and countryaddressstructure
+    num_addresses = Addresses.objects.all().count()
+    num_countryAddressStructure = CountryAddressStructure.objects.all().count()
+
+    context = {
+        'num_addresses': num_addresses,
+        'num_countryAddressStructure': num_countryAddressStructure,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
