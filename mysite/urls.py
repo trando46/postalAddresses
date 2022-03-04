6216ce9a941b29924bdfd28d469d0bb2320d106f
roopinij17 <roopinijreddy@gmail.com>
2022-03-03 20:08:48 -0600

    Deleted old controller.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapi import controllerC
+from myapi import controller
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
