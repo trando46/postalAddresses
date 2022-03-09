6e96e26ebdde1a7f3da67cb8df8b0718f656ca31
roopinij17 <roopinijreddy@gmail.com>
2022-03-08 21:53:13 -0600

    removed render in  apiController.py

--- myapi/apiController.py
+++ myapi/apiController.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from postalAddress import CountryAddressStructureRepository
 from postalAddress import AddressesRespository
 from postalAddress import StateRespository
+from django.http import HttpResponseBadRequest
 
 class Controller:
 
 
     def Get_States(request):
         return StateRespository.index(request)
+
     def Add_States(request):
         #list for adding Address
         states = []
@@ -21,23 +23,23 @@ class Controller:
                     states.append(request.POST.get('state_id'))
                 else:
                     messages.error(request,'Enter valid state ID')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('country_id'))) >=1:
                     states.append(request.POST.get('country_id'))
                 else:
                     messages.error(request,'Enter valid Country ID')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('state'))) >=1:
                     states.append(request.POST.get('state'))
                 else:
                     messages.error(request,'Enter valid state')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 return StateRespository.create(request,states)
         else:
-            return render(request,'insert.html')
+            return HttpResponseBadRequest("We cannot process the request")
 
 #get for country
     def Get_Country(request):
@@ -55,29 +57,29 @@ class Controller:
                     country.append(request.POST.get('country_id'))
                 else:
                     messages.error(request,'Enter valid Country ID')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('country_name'))) >=1:
                     country.append(request.POST.get('country_name'))
                 else:
                     messages.error(request,'Enter valid Country name')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('country_iso'))) >=1:
                     country.append(request.POST.get('country_iso'))
                 else:
                     messages.error(request,'Enter valid Country ISO')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('address_format'))) >=1:
                     country.append(request.POST.get('address_format'))
                 else:
                     messages.error(request,'Enter valid Address format')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 return CountryAddressStructureRepository.create(request,country)
         else:
-            return render(request,'insert.html')
+            return HttpResponseBadRequest("We cannot process the request")
 
 #get for Address
     def Get_Address(request):
@@ -96,22 +98,22 @@ class Controller:
                     address.append(request.POST.get('address_id'))
                 else:
                     messages.error(request,'Enter valid Address ID')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('country_id'))) >=1:
                     address.append(request.POST.get('country_id'))
                 else:
                     messages.error(request,'Enter valid Country ID')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 if (len(request.POST.get('addressLine'))) >=1:
                     address.append(request.POST.get('addressLine'))
                 else:
                     messages.error(request,'Enter valid Address Line')
-                    return render(request,'insert.html')
+                    #return render(request,'insert.html')
 
                 return AddressesRespository.create(request,address)
         else:
-            return render(request,'insert.html')
+            return HttpResponseBadRequest("We cannot process the request")
 
 
